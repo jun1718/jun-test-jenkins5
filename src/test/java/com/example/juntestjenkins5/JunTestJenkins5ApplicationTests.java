8831999ae0847548e5c5c7e7d37a42d1f0bc621a
jun1718 <gbeovhsqhtka@naver.com>
2022-12-24 18:36:50 +0900

    Initial commit

---
+++ src/test/java/com/example/juntestjenkins5/JunTestJenkins5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juntestjenkins5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunTestJenkins5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
